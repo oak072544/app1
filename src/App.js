8424e7a5183a2eeea7c72a1b3d19c8275d2c1546
oak072544 <oak072544@gmail.com>
2023-08-11 14:26:09 +0700

    fc_1
    function component 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 //import logo from './nyan_cat.svg';
-import "./App.css";
+//import "./App.css";
 import React from "react";
-import "./style.css";
+//import "./style.css";
+import { Header, Content, Footer } from "./func-components";
 
 function App() {
   return (
     <>
-      <h2>DeveloperThai.com</h2>
-      <div className="title">React & React Native</div>
+      <Header />
+      <Content />
+      <Footer />
     </>
   );
 }
