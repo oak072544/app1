ebf3bf8cc547c93a2a77ea28ad7745e7ea764d3f
oak072544 <oak072544@gmail.com>
2023-08-11 14:49:12 +0700

    class_component
    class component example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 //import logo from './nyan_cat.svg';
 //import "./App.css";
-import React from "react";
 //import "./style.css";
+import React from "react";
 import { Header, Content, Footer } from "./func-components";
+import Calender from "./class-components";
 
 function App() {
   return (
     <>
       <Header />
+      <p><center><Calender/></center></p>
       <Content />
       <Footer />
     </>
