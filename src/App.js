930933faf207e001ae22b6d8f0d0da7d3e394732
oak072544 <oak072544@gmail.com>
2023-08-18 15:36:42 +0700

    event data
    event data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-//import logo from './nyan_cat.svg';
-//import "./App.css";
-//import "./style.css";
 import React from "react";
-/*
-import { Header, Content, Footer } from "./func-components";
-import Calender from "./class-components";
-*/
-//import Banner from "./banner";
-import { Calculator2 } from "./calculator";
+
+import { EventData2 } from "./event-data";
 
 export default function App(){
-  return <Calculator2/>;
+  return <EventData2/>;
 }
