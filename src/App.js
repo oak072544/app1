acd338c1c819e726d62dd3772678a76a2a214dd8
oak072544 <oak072544@gmail.com>
2023-09-08 22:37:36 +0700

    context function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { userContext } from './context'
-import Header from './context-header'
+import Content from './context-content'
 export default function App() {
     return (
         <userContext.Provider value={'Tom Jerry'}>
-        <Header/>
+        <Content/>
         </userContext.Provider>
     ) 
 }
