9091c35af16c65f15470605de01c3c6d3fc92890
oak072544 <oak072544@gmail.com>
2023-09-08 22:40:33 +0700

    context state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import { userContext } from "./context";
-import Content from "./context-content";
-import Header from "./context-header";
+import React from 'react'
+import { userContext } from './context'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
 export default function App() {
-  return (
-    <userContext.Provider value={"Tom Jerry"}>
-      <Header /> 
-      <Content />
+    let [user, setUser] = React.useState()
+    return (
+    <userContext.Provider value={[user, setUser]}>
+    <Header2/>
+    <Content2/>
     </userContext.Provider>
-  );
-}
+    ) 
+    }
+    
