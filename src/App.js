033cc43799a011d29dfafe87e1ae1aa0a930f27d
oak072544 <oak072544@gmail.com>
2023-09-08 22:39:26 +0700

    Use Together

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import { userContext } from './context'
-import Content from './context-content'
+import React from "react";
+import { userContext } from "./context";
+import Content from "./context-content";
+import Header from "./context-header";
 export default function App() {
-    return (
-        <userContext.Provider value={'Tom Jerry'}>
-        <Content/>
-        </userContext.Provider>
-    ) 
+  return (
+    <userContext.Provider value={"Tom Jerry"}>
+      <Header /> 
+      <Content />
+    </userContext.Provider>
+  );
 }
