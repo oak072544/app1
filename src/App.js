3329c9ebc6b72b89ea44e6ecc0799ad0b8bfdc01
oak072544 <oak072544@gmail.com>
2023-09-29 13:14:40 +0700

    form search 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Router3 from "./router3";
+import FormSearch from "./form-search";
 export default function App() {
-  return <Router3 />;
+  return <FormSearch />;
 }
